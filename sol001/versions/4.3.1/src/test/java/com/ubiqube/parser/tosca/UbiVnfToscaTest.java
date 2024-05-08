46a5113c01436853ac557e404c472912f1b1acbd
Olivier Vignaud <ovi@ubiqube.com>
2024-05-08 16:51:46 +0200

    ♻️ tosca-4.3.1: Change OrikaMapper to generic ToscaMapper.

--- sol001/versions/4.3.1/src/test/java/com/ubiqube/parser/tosca/UbiVnfToscaTest.java
+++ sol001/versions/4.3.1/src/test/java/com/ubiqube/parser/tosca/UbiVnfToscaTest.java
@@ -44,13 +44,12 @@ import org.slf4j.LoggerFactory;
 
 import com.ubiqube.parser.tosca.ZipUtil.Entry;
 import com.ubiqube.parser.tosca.api.ToscaApi;
+import com.ubiqube.parser.tosca.api.ToscaMapper;
 import com.ubiqube.parser.tosca.objects.tosca.nodes.nfv.VnfExtCp;
 import com.ubiqube.parser.tosca.objects.tosca.nodes.nfv.VnfVirtualLink;
 import com.ubiqube.parser.tosca.objects.tosca.nodes.nfv.vdu.Compute;
 import com.ubiqube.parser.tosca.objects.tosca.policies.nfv.VduScalingAspectDeltas;
 import com.ubiqube.parser.tosca.objects.tosca.policies.nfv.VnfIndicator;
-
-import ma.glasnost.orika.MapperFactory;
 
 class UbiVnfToscaTest {
 
@@ -61,8 +60,8 @@ class UbiVnfToscaTest {
 	private final ToscaApi toscaApi;
 
 	public UbiVnfToscaTest() {
-		final MapperFactory mapperFactory = Utils.createMapperFactory();
-		toscaApi = new ToscaApi(this.getClass().getClassLoader(), mapperFactory.getMapperFacade());
+		final ToscaMapper mapperFactory = Utils.createMapperFactory();
+		toscaApi = new ToscaApi(this.getClass().getClassLoader(), mapperFactory);
 	}
 
 	@Test
