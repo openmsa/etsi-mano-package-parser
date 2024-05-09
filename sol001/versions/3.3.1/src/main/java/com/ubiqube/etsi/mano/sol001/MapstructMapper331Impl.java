cbde5182bc8d209f3cc2c517ef467c635b813238
Olivier Vignaud <ovi@ubiqube.com>
2024-05-09 19:56:51 +0200

    ✨ 3.3.1: Add tosca.nodes.Compute mapping.

--- sol001/versions/3.3.1/src/main/java/com/ubiqube/etsi/mano/sol001/MapstructMapper331Impl.java
+++ sol001/versions/3.3.1/src/main/java/com/ubiqube/etsi/mano/sol001/MapstructMapper331Impl.java
@@ -90,6 +90,7 @@ public class MapstructMapper331Impl implements ToscaMapper {
 		case "com.ubiqube.parser.tosca.objects.tosca.policies.nfv.SupportedVnfInterface" -> policyMapper.mapToSupportedVnfInterface((tosca.policies.nfv.SupportedVnfInterface) arg);
 		case "com.ubiqube.parser.tosca.objects.tosca.policies.nfv.SecurityGroupRule" -> policyMapper.mapToSecurityGroupRule((tosca.policies.nfv.SecurityGroupRule) arg);
 		case "com.ubiqube.parser.tosca.objects.tosca.policies.nfv.VnfPackageChange" -> policyMapper.mapToVnfPackageChange((tosca.policies.nfv.VnfPackageChange) arg);
+		case "com.ubiqube.parser.tosca.objects.tosca.nodes.Compute" -> nodeMapper.mapToCompute((tosca.nodes.Compute) arg);
 		default -> throw new IllegalArgumentException("Unexpected value: " + destClass);
 		};
 		return (U) r;
