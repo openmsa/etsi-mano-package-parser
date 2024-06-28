35d5f025afd4802976278e4eeee992112436b466
Olivier Vignaud <ovi@ubiqube.com>
2024-06-28 11:16:32 +0200

    🚨 sol001-3.6.1: Remove mapping warnings.

--- sol001/versions/3.6.1/src/main/java/com/ubiqube/etsi/mano/sol001/v361/mapper/NodeMapper.java
+++ sol001/versions/3.6.1/src/main/java/com/ubiqube/etsi/mano/sol001/v361/mapper/NodeMapper.java
@@ -22,6 +22,7 @@ import org.mapstruct.Mapping;
 import com.ubiqube.parser.tosca.objects.tosca.datatypes.nfv.L3AddressData;
 import com.ubiqube.parser.tosca.objects.tosca.datatypes.nfv.NsProfile;
 import com.ubiqube.parser.tosca.objects.tosca.datatypes.nfv.ServiceData;
+import com.ubiqube.parser.tosca.objects.tosca.datatypes.nfv.VirtualMemory;
 import com.ubiqube.parser.tosca.objects.tosca.datatypes.nfv.VirtualNetworkInterfaceRequirements;
 import com.ubiqube.parser.tosca.objects.tosca.nodes.nfv.Forwarding;
 import com.ubiqube.parser.tosca.objects.tosca.nodes.nfv.NFP;
@@ -85,6 +86,9 @@ public interface NodeMapper {
 	VirtualNetworkInterfaceRequirements mapToVirtualNetworkInterfaceRequirements(tosca.datatypes.nfv.VirtualNetworkInterfaceRequirements o);
 
 	Compute mapToCompute(tosca.nodes.nfv.vdu.Compute o);
+
+	@Mapping(target = "hugePagesRequirements", ignore = true)
+	VirtualMemory mapToVirtualMemory(tosca.datatypes.nfv.VirtualMemory o);
 
 	@Mapping(target = "perVnfcInstance", ignore = true)
 	VirtualBlockStorage mapToVirtualBlockStorage(tosca.nodes.nfv.vdu.VirtualBlockStorage o);
